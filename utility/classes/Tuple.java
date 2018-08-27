af42fb4a892853a7f82c52cd395b29a04642d5f5
Ayoub Falah <ayoub_falah@hotmail.de>
2018-08-27 15:26:55 +0200

    Designing a generic class for representing tuples

---
+++ utility/classes/Tuple.java
@@ -0,0 +1,15 @@
+package utility.classes;
+
+/**
+ *
+ * @author ayoubfalah
+ * @param <T>
+ */
+public class Tuple<T>
+{
+    private final T[] coordinates;
+
+    public Tuple(T[] coordinates) {
+        this.coordinates = coordinates;
+    }
+}
